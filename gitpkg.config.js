ba71456475ef5f151dada2e147d2e090681c7fbc
Tuyen Nguyen <tuyennguyen@Tuyen-Nguyens-Macbook.local>
2022-01-13 10:06:08 +0700

    update repo path

--- gitpkg.config.js
+++ gitpkg.config.js
@@ -1,4 +1,4 @@
 module.exports = () => ({
-    registry: "https://github.com/101digital/customer-component.git",
+    registry: "https://github.com/101digital/invoice-component.git",
     getTagName: (pkg) => `${pkg.version}`,
   });
